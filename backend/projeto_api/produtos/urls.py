6ce39b07bf25870d51aa79c7414ae8dbf34e605e
PedroHGFonsecadev2024 <pedrofonseca.dev2024@gmail.com>
2025-09-11 13:29:03 -0300

    Backend finalizado

--- backend/projeto_api/produtos/urls.py
+++ backend/projeto_api/produtos/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
      path('categorias/', views.CategoriaList.as_view(), name= 'categoria-list'),
-     path('categorias/<int/pk>/', views.CategoriaDetail.as_view, name='categoria-detail'),
+     path('categorias/<int:pk>/', views.CategoriaDetail.as_view, name='categoria-detail'),
      path('produtos/', views.ProdutoList.as_view(), name='produto-list'),
-     path('produtos/<int/pk>/', views.ProdutoDetail.as_view(), name='produto-detail' ),
+     path('produtos/<int:pk>/', views.ProdutoDetail.as_view(), name='produto-detail' ),
 ]
